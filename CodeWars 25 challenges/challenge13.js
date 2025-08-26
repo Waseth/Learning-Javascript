f36a81d4167a775d9566a5b1e3133142b068f20d
Waseth <wasethsapriso@gmail.com>
2025-08-26 16:37:59 +0300

    more challenges completed

---
+++ CodeWars 25 challenges/challenge13.js
@@ -0,0 +1,6 @@
+function removeChar(str){
+  let newStr = str.slice(1,-1);
+  return newStr;
+};
+
+// I have learned how slice() works.The first number in the brackets is the starting index and it is inclusive while the last one is exclusive and that's where basically we stop slicing from.
